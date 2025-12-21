8b033d7bc5d1a3a5b79b41642ebbca249293a256
Erkan Gülbaş <erkan.glbas@gmail.com>
2025-12-22 01:49:36 +0300

    Vite config eklendi

---
+++ frontend/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist', // Çıktı klasörümüzün adı
+    emptyOutDir: true, // Her build'de temizle
+  },
+});
